df97c01a7d2d34264e10e2c109ba8ccf7f491357
xiafengty <1206332706@qq.com>
2018-08-28 11:13:47 +0800

    eslint配置完成

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,7 +11,7 @@ module.exports = {
   extends: [
     // https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
     // consider switching to `plugin:vue/strongly-recommended` or `plugin:vue/recommended` for stricter rules.
-    'plugin:vue/essential', 
+    'plugin:vue/essential',
     // https://github.com/standard/standard/blob/master/docs/RULES-en.md
     'standard'
   ],
@@ -20,10 +20,25 @@ module.exports = {
     'vue'
   ],
   // add your custom rules here
-  rules: {
+  'rules': {
+    // allow paren-less arrow functions
+    'arrow-parens': 0,
     // allow async-await
-    'generator-star-spacing': 'off',
+    'generator-star-spacing': 0,
     // allow debugger during development
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
+    "no-unused-vars": [2, {
+      // 允许声明未使用变量
+      "vars": "local",
+      // 参数不检查
+      "args": "none"
+    }],
+    // 关闭语句强制分号结尾
+    "semi": [0],
+    //空行最多不能超过100行
+    "no-multiple-empty-lines": [0, {"max": 100}],
+    //关闭禁止混用tab和空格
+    "no-mixed-spaces-and-tabs": [0],
+    "spaced-comment": 0,//注释风格要不要有空格什么的
   }
 }
--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,14 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld/>
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
+
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+
 }
 </script>
 
